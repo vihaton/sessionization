40633d9e5e95e42c3a723d58c698b3a70bc377ba
Vili <vili.hatonen@helsinki.fi>
2019-01-18 21:30:03 +0200

    reading events from stdin adds event count in a session

--- source/sessionize.py
+++ source/sessionize.py
@@ -1,6 +1,8 @@
 import sys
 import json
 
+#closedsessions = {} #to move data from the open ones, prevents extra work
+opensessions = {}
 """
 {
   "timestamp": 123,
@@ -12,12 +14,12 @@ import json
 class Sessio(object):
     sc = ""
 
-    def __init__(self, user_id, content_id, session_start):
+    def __init__(self, event):
         global sc
         sc = {} # session content
-        sc["user_id"] = user_id
-        sc["content_id"] = content_id
-        sc["session_start"] = session_start
+        sc["user_id"] = event["user_id"]
+        sc["content_id"] = event["content_id"]
+        sc["session_start"] = event["timestamp"]
         sc["session_end"] = -1
         sc["total_time"] = 0
         sc["track_playtime"] = 0
@@ -27,9 +29,21 @@ class Sessio(object):
     def print_this(self):
         print(sc)
 
+    def add_event(self, event):
+        #TODO add all other status updates
+        sc["event_count"] += 1
+
 def process_event(event):
-    #do stuff to address event to an session
-    print(event)
+    #TODO if event is about starting a session then the previous session is closed
+
+    #does event belong to some S
+    if event["user_id"] in opensessions.keys():
+        #update the session
+        s = opensessions[event["user_id"]]
+        s.add_event(event)
+
+    else:   #create new session
+        opensessions[event["user_id"]] = Sessio(event) # assumes that one user can have one session open
 
 def parse_input(line):
     try:
@@ -42,9 +56,10 @@ def main():
     for line in sys.stdin:
         event = parse_input(line)
         process_event(event)
-    
-    s = Sessio(123, 321, 1)
-    s.print_this()
+        print("***\n")
+        for s in opensessions.values():
+            s.print_this()
+
 
 if __name__ is "__main__":
     main()
