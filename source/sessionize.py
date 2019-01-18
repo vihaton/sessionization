f1999c53802b298446894fd34946f749d99ea718
Vili <vili.hatonen@helsinki.fi>
2019-01-18 22:07:04 +0200

    sessions that time out are closed and printed

--- source/sessionize.py
+++ source/sessionize.py
@@ -4,6 +4,8 @@ __author__ = "Vili Hätönen"
 
 #closedsessions = {} #to move data from the open ones, prevents extra work
 opensessions = {}
+lts = -1 # the Latest TimeStamp for checking timeouts
+
 """
 {
   "timestamp": 123,
@@ -18,8 +20,9 @@ class Sessio(object):
         self.sc = {} # session content
         self.sc["user_id"] = event["user_id"]
         self.sc["content_id"] = event["content_id"]
-        self.sc["session_start"] = event["timestamp"]
-        self.sc["session_end"] = -1
+        ts = event["timestamp"]
+        self.sc["session_start"] = ts
+        self.sc["session_end"] = ts
         self.sc["total_time"] = 0
         self.sc["track_playtime"] = 0
         self.sc["event_count"] = 1
@@ -29,10 +32,30 @@ class Sessio(object):
         print(self.sc)
 
     def add_event(self, event):
-        #TODO add all other status updates
         self.sc["event_count"] += 1
+        ts = event["timestamp"]
+        self.sc["session_end"] = ts
+        self.sc["total_time"] = ts - self.sc["session_start"] # this is the way total time is approximated in the instructions, ie. exclude the 60s wait for timeout
+
+        #TODO add all other status updates
+
+def check_timeouts():
+    global lts
+
+    topop = []
+    for k in opensessions.keys():
+        s = opensessions[k]
+        if lts >= 60 + int(s.sc["session_end"]): #if the latest event on this session was over a minute ago
+            topop.append(k)      #add this to ones to be closed
+
+    # close the timed out sessions
+    for k in topop:
+        print(opensessions.pop(k).print_this())
 
 def process_event(event):
+    global lts
+    lts = event["timestamp"]
+
     #TODO if event is about starting a session then the previous session is closed
 
     #does event belong to some S
@@ -56,9 +79,11 @@ def main():
     for line in sys.stdin:
         event = parse_input(line)
         process_event(event)
-        print("\n***")
-        for s in opensessions.values():
-            s.print_this()
+        check_timeouts()
 
+    print("open sessions in the end:")
+    for s in opensessions.values():
+        s.print_this()
+        
 if __name__ == "__main__":
     main()
